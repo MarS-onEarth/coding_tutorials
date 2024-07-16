1e50232293cb5043ca0c2b0522b65818553c4f71
Martin <martin.schadow@gmail.com>
2024-07-16 10:34:49 -0700

    dtc_finished
    erstes Spiel fertig

--- Godot/Dodge_the_Creeps/scripts/main.gd
+++ Godot/Dodge_the_Creeps/scripts/main.gd
@@ -18,12 +18,18 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
 	$player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get ready...")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate() # Create a new instance of the Mob scene.
@@ -47,6 +53,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
